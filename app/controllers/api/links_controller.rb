bbc5cf4cdded19caf880ab9af1f9eee8591c70da
Henrique Breim <henrique@breim.com.br>
2024-02-09 20:47:37 -0300

    feat: generate links controller

---
+++ app/controllers/api/links_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Api
+  class LinksController < ApplicationController
+    respond_to :json
+
+    def index
+      @links = Link.all
+      respond_with(@links)
+    end
+
+    def show
+      @link = Link.find(params[:id])
+      respond_with(@link)
+    end
+
+    def create
+      @link = Link.new(link_params)
+      if @link.save
+        respond_with(@link)
+      else
+        respond_with(@link.errors, status: :unprocessable_entity)
+      end
+    end
+
+    def update
+      @link = Link.find(params[:id])
+      if @link.update(link_params)
+        respond_with(@link)
+      else
+        respond_with(@link.errors, status: :unprocessable_entity)
+      end
+    end
+
+    def destroy
+      @link = Link.find(params[:id])
+      @link.destroy
+      respond_with(@link)
+    end
+
+    private
+
+    def link_params
+      params.require(:link).permit(:original_url)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api, path: 'api/v1', defaults: { format: :json } do
+    resources :links
+  end
 end
