b759142d01755c758fabd2642682e317d271364d
Henrique Breim <henrique@breim.com.br>
2024-02-09 22:07:02 -0300

    feat: create a uniq token to handle shortcode

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,13 @@
 
 class Link < ApplicationRecord
   has_many :analytics, dependent: :destroy
+  validates :original_url, presence: true
+
+  before_create :build_token
+
+  private
+
+  def build_token
+    self.token = SecureRandom.urlsafe_base64(5)
+  end
 end
